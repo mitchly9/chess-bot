8ec86d2c15b8db540ee207558deb3ff9f41b8411
lymit000 <lymit000@cs.washington.edu>
2024-03-30 19:51:47 -0700

    All basic piece functinoality done

--- src/app/components/Board.js
+++ src/app/components/Board.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Board() {
+  const [turn, setTurn] = useState(1);
   const [selectedTile, setSelectedTile] = useState("");
   const board = [
     ["18", "28", "38", "48", "58", "68", "78", "88"],
@@ -93,34 +94,67 @@ export default function Board() {
     if (tile === selectedTile) {
       setSelectedTile("");
     } else {
-      switch (boardState[tile]) {
-        case "px":
-        case "Px":
-        case "rx":
-        case "Rx":
-        case "nx":
-        case "Nx":
-        case "bx":
-        case "Bx":
-        case "qx":
-        case "Qx":
-        case "m":
-          move(tile);
-          break;
-        case "P":
-          showPawnMoves(tile, true);
-          break;
-        case "p":
-          showPawnMoves(tile, false);
-          break;
-        case "R":
-          showRookMoves(tile, true);
-          break;
-        case "r":
-          showRookMoves(tile, false);
-          break;
-        default:
-          break;
+      if (turn % 2 === 0) {
+        switch (boardState[tile]) {
+          case "px":
+          case "rx":
+          case "nx":
+          case "bx":
+          case "qx":
+          case "m":
+            move(tile);
+            break;
+          case "p":
+            showPawnMoves(tile, false);
+            break;
+          case "r":
+            showRookMoves(tile, false);
+            break;
+          case "n":
+            shownKnightMoves(tile, false);
+            break;
+          case "b":
+            showBishopMoves(tile, false);
+          case "q":
+            showQueenMoves(tile, false);
+            break;
+          case "k":
+            showKingMoves(tile, false);
+            false;
+          default:
+            break;
+        }
+      } else {
+        switch (boardState[tile]) {
+          case "Px":
+          case "Rx":
+          case "Nx":
+          case "Bx":
+          case "Qx":
+          case "m":
+            move(tile);
+            break;
+          case "P":
+            showPawnMoves(tile, true);
+            break;
+          case "R":
+            showRookMoves(tile, true);
+            break;
+          case "N":
+            shownKnightMoves(tile, true);
+            break;
+          case "B":
+            showBishopMoves(tile, true);
+            break;
+          case "Q":
+            showQueenMoves(tile, true);
+            break;
+          case "K":
+            showKingMoves(tile, true);
+            break;
+          default:
+            break;
+        }
       }
     }
   }
@@ -184,6 +218,7 @@ export default function Board() {
     setBoardState((boardState) => ({ ...boardState, [selectedTile]: "-" }));
     setSelectedTile("");
     setPossibleMoves([]);
+    setTurn((currentTurn) => currentTurn + 1);
   }
 
   function attackPiece(attackedTile, isWhite) {
@@ -205,17 +240,6 @@ export default function Board() {
     setSelectedTile(tile);
     let verticalMove = isWhite ? parseInt(tile[1]) + 1 : parseInt(tile[1]) - 1;
 
-    if ((isWhite && tile[1] === "2") || (!isWhite && tile[1] === "7")) {
-      let newTile = tile[0] + (isWhite ? verticalMove + 1 : verticalMove - 1);
-      if (boardState[newTile] === "-") {
-        setPossibleMoves((currentPossibleMoves) => [
-          ...currentPossibleMoves,
-          [newTile],
-        ]);
-        setBoardState((boardState) => ({ ...boardState, [newTile]: "m" }));
-      }
-    }
-
     let newTile = tile[0] + verticalMove;
     if (boardState[newTile] === "-") {
       setPossibleMoves((currentPossibleMoves) => [
@@ -223,6 +247,16 @@ export default function Board() {
         [newTile],
       ]);
       setBoardState((boardState) => ({ ...boardState, [newTile]: "m" }));
+      if ((isWhite && tile[1] === "2") || (!isWhite && tile[1] === "7")) {
+        let newTile = tile[0] + (isWhite ? verticalMove + 1 : verticalMove - 1);
+        if (boardState[newTile] === "-") {
+          setPossibleMoves((currentPossibleMoves) => [
+            ...currentPossibleMoves,
+            [newTile],
+          ]);
+          setBoardState((boardState) => ({ ...boardState, [newTile]: "m" }));
+        }
+      }
     }
 
     let horizontalMove = parseInt(tile[0]) - 1;
@@ -234,10 +268,7 @@ export default function Board() {
     attackPiece(rightAttack, isWhite);
   }
 
-  function showRookMoves(tile, isWhite) {
-    setSelectedTile(tile);
-    const ownPieces = isWhite ? whitePieces : blackPieces;
-
+  function leftMove(tile, isWhite, ownPieces) {
     let leftMove = parseInt(tile[0]) - 1;
     let leftAttack = leftMove + tile[1];
     while (leftMove > 0) {
@@ -259,7 +290,9 @@ export default function Board() {
       leftMove--;
       leftAttack = leftMove + tile[1];
     }
+  }
 
+  function rightMove(tile, isWhite, ownPieces) {
     let rightMove = parseInt(tile[0]) + 1;
     let rightAttack = rightMove + tile[1];
     while (rightMove < 9) {
@@ -281,7 +314,34 @@ export default function Board() {
       rightMove++;
       rightAttack = rightMove + tile[1];
     }
+  }
 
+  function downMove(tile, isWhite, ownPieces) {
+    let downMove = parseInt(tile[1]) - 1;
+    let downAttack = tile[0] + downMove;
+    while (downMove > 0) {
+      const currentDownAttack = downAttack;
+      if (
+        ownPieces.includes(boardState[currentDownAttack]) ||
+        attackPiece(currentDownAttack, isWhite)
+      ) {
+        break;
+      }
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [currentDownAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [currentDownAttack]: "m",
+      }));
+
+      downMove = downMove - 1;
+      downAttack = tile[0] + downMove;
+    }
+  }
+
+  function upMove(tile, isWhite, ownPieces) {
     let upMove = parseInt(tile[1]) + 1;
     let upAttack = tile[0] + upMove;
     while (upMove < 9) {
@@ -304,28 +364,406 @@ export default function Board() {
       upMove++;
       upAttack = tile[0] + upMove;
     }
+  }
 
+  function showRookMoves(tile, isWhite) {
+    setSelectedTile(tile);
+    const ownPieces = isWhite ? whitePieces : blackPieces;
+
+    leftMove(tile, isWhite, ownPieces);
+    rightMove(tile, isWhite, ownPieces);
+    upMove(tile, isWhite, ownPieces);
+    downMove(tile, isWhite, ownPieces);
+  }
+
+  function shownKnightMoves(tile, isWhite) {
+    setSelectedTile(tile);
+    const ownPieces = isWhite ? whitePieces : blackPieces;
+
+    let upUpMove = parseInt(tile[1]) + 2;
+    let downDownMove = parseInt(tile[1]) - 2;
+    let leftMove = parseInt(tile[0]) - 1;
+    let rightMove = parseInt(tile[0]) + 1;
+
+    let leftLeftMove = parseInt(tile[0]) - 2;
+    let rightRightMove = parseInt(tile[0]) + 2;
+    let upMove = parseInt(tile[1]) + 1;
     let downMove = parseInt(tile[1]) - 1;
-    let downAttack = tile[0] + downMove;
-    while (downMove > 0) {
-      const currentDownAttack = downAttack;
+
+    let upLeftAttack = leftMove.toString() + upUpMove;
+    let upRightAttack = rightMove.toString() + upUpMove;
+    let downLeftAttack = leftMove.toString() + downDownMove;
+    let downRightAttack = rightMove.toString() + downDownMove;
+
+    let leftUpAttack = leftLeftMove.toString() + upMove;
+    let leftDownAttack = leftLeftMove.toString() + downMove;
+    let rightUpAttack = rightRightMove.toString() + upMove;
+    let rightDownAttack = rightRightMove.toString() + downMove;
+
+    if (
+      !ownPieces.includes(boardState[upLeftAttack]) &&
+      !attackPiece(upLeftAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [upLeftAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [upLeftAttack]: "m",
+      }));
+    }
+    if (
+      !ownPieces.includes(boardState[upRightAttack]) &&
+      !attackPiece(upRightAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [upRightAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [upRightAttack]: "m",
+      }));
+    }
+    if (
+      !ownPieces.includes(boardState[downLeftAttack]) &&
+      !attackPiece(downLeftAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [downLeftAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [downLeftAttack]: "m",
+      }));
+    }
+    if (
+      !ownPieces.includes(boardState[downRightAttack]) &&
+      !attackPiece(downRightAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [downRightAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [downRightAttack]: "m",
+      }));
+    }
+    if (
+      !ownPieces.includes(boardState[leftUpAttack]) &&
+      !attackPiece(leftUpAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [leftUpAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [leftUpAttack]: "m",
+      }));
+    }
+    if (
+      !ownPieces.includes(boardState[leftDownAttack]) &&
+      !attackPiece(leftDownAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [leftDownAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [leftDownAttack]: "m",
+      }));
+    }
+    if (
+      !ownPieces.includes(boardState[rightUpAttack]) &&
+      !attackPiece(rightUpAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [rightUpAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [rightUpAttack]: "m",
+      }));
+    }
+    if (
+      !ownPieces.includes(boardState[rightDownAttack]) &&
+      !attackPiece(rightDownAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [rightDownAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [rightDownAttack]: "m",
+      }));
+    }
+  }
+
+  function upRightMove(tile, isWhite, ownPieces) {
+    let upMove = parseInt(tile[1]) + 1;
+    let rightMove = parseInt(tile[0]) + 1;
+    let upRightAttack = rightMove.toString() + upMove;
+    while (upMove < 9 && rightMove < 9) {
+      const currentUpRightAttack = upRightAttack;
       if (
-        ownPieces.includes(boardState[currentDownAttack]) ||
-        attackPiece(currentDownAttack, isWhite)
+        ownPieces.includes(boardState[currentUpRightAttack]) ||
+        attackPiece(currentUpRightAttack, isWhite)
       ) {
         break;
       }
       setPossibleMoves((currentPossibleMoves) => [
         ...currentPossibleMoves,
-        [currentDownAttack],
+        [currentUpRightAttack],
       ]);
       setBoardState((boardState) => ({
         ...boardState,
-        [currentDownAttack]: "m",
+        [currentUpRightAttack]: "m",
       }));
+      upMove++;
+      rightMove++;
+      upRightAttack = rightMove.toString() + upMove;
+    }
+  }
 
+  function downRightMove(tile, isWhite, ownPieces) {
+    let downMove = parseInt(tile[1]) - 1;
+    let rightMove = parseInt(tile[0]) + 1;
+    let downRightAttack = rightMove.toString() + downMove;
+    while (downMove > 0 && rightMove < 9) {
+      const currentDownRightAttack = downRightAttack;
+
+      if (
+        ownPieces.includes(boardState[currentDownRightAttack]) ||
+        attackPiece(currentDownRightAttack, isWhite)
+      ) {
+        break;
+      }
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [currentDownRightAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [currentDownRightAttack]: "m",
+      }));
       downMove = downMove - 1;
-      downAttack = tile[0] + downMove;
+      rightMove++;
+      downRightAttack = rightMove.toString() + downMove;
+    }
+  }
+
+  function upLeftMove(tile, isWhite, ownPieces) {
+    let upMove = parseInt(tile[1]) + 1;
+    let leftMove = parseInt(tile[0]) - 1;
+    let upLeftAttack = leftMove.toString() + upMove;
+    while (upMove < 9 && leftMove > 0) {
+      const currentLeftUpAttack = upLeftAttack;
+
+      if (
+        ownPieces.includes(boardState[currentLeftUpAttack]) ||
+        attackPiece(currentLeftUpAttack, isWhite)
+      ) {
+        break;
+      }
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [currentLeftUpAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [currentLeftUpAttack]: "m",
+      }));
+      upMove++;
+      leftMove = leftMove - 1;
+      upLeftAttack = leftMove.toString() + upMove;
+    }
+  }
+
+  function downLeftMove(tile, isWhite, ownPieces) {
+    let downMove = parseInt(tile[1]) - 1;
+    let leftMove = parseInt(tile[0]) - 1;
+    let downLeftAttack = leftMove.toString() + downMove;
+    while (downMove > 0 && leftMove > 0) {
+      const currentDownLeftAttack = downLeftAttack;
+
+      if (
+        ownPieces.includes(boardState[currentDownLeftAttack]) ||
+        attackPiece(currentDownLeftAttack, isWhite)
+      ) {
+        break;
+      }
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [currentDownLeftAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [currentDownLeftAttack]: "m",
+      }));
+      downMove = downMove - 1;
+      leftMove = leftMove - 1;
+      downLeftAttack = leftMove.toString() + downMove;
+    }
+  }
+
+  function showBishopMoves(tile, isWhite) {
+    setSelectedTile(tile);
+    const ownPieces = isWhite ? whitePieces : blackPieces;
+
+    upRightMove(tile, isWhite, ownPieces);
+    downRightMove(tile, isWhite, ownPieces);
+    upLeftMove(tile, isWhite, ownPieces);
+    downLeftMove(tile, isWhite, ownPieces);
+  }
+
+  function showQueenMoves(tile, isWhite) {
+    setSelectedTile(tile);
+    const ownPieces = isWhite ? whitePieces : blackPieces;
+
+    leftMove(tile, isWhite, ownPieces);
+    upMove(tile, isWhite, ownPieces);
+    rightMove(tile, isWhite, ownPieces);
+    downMove(tile, isWhite, ownPieces);
+    upLeftMove(tile, isWhite, ownPieces);
+    upRightMove(tile, isWhite, ownPieces);
+    downLeftMove(tile, isWhite, ownPieces);
+    downRightMove(tile, isWhite, ownPieces);
+  }
+
+  function showKingMoves(tile, isWhite) {
+    setSelectedTile(tile);
+    const ownPieces = isWhite ? whitePieces : blackPieces;
+
+    let upMove = parseInt(tile[1]) + 1;
+    let leftMove = parseInt(tile[0]) - 1;
+    let rightMove = parseInt(tile[0]) + 1;
+    let downMove = parseInt(tile[1]) - 1;
+
+    let upAttack = tile[0] + upMove;
+    if (
+      !ownPieces.includes(boardState[upAttack]) &&
+      !attackPiece(upAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [upAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [upAttack]: "m",
+      }));
+    }
+
+    let upLeftAttack = leftMove.toString() + upMove;
+    if (
+      !ownPieces.includes(boardState[upLeftAttack]) &&
+      !attackPiece(upLeftAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [upLeftAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [upLeftAttack]: "m",
+      }));
+    }
+
+    let upRightAttack = rightMove.toString() + upMove;
+    if (
+      !ownPieces.includes(boardState[upRightAttack]) &&
+      !attackPiece(upRightAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [upRightAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [upRightAttack]: "m",
+      }));
+    }
+
+    let leftMoveAttack = leftMove + tile[1];
+    if (
+      !ownPieces.includes(boardState[leftMoveAttack]) &&
+      !attackPiece(leftMoveAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [leftMoveAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [leftMoveAttack]: "m",
+      }));
+    }
+
+    let rightMoveAttack = rightMove + tile[1];
+    if (
+      !ownPieces.includes(boardState[rightMoveAttack]) &&
+      !attackPiece(rightMoveAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [rightMoveAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [rightMoveAttack]: "m",
+      }));
+    }
+
+    let downMoveAttack = tile[0] + downMove;
+    if (
+      !ownPieces.includes(boardState[downMoveAttack]) &&
+      !attackPiece(downMoveAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [downMoveAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [downMoveAttack]: "m",
+      }));
+    }
+
+    let downRightAttack = rightMove.toString() + downMove;
+    if (
+      !ownPieces.includes(boardState[downRightAttack]) &&
+      !attackPiece(downRightAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [downRightAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [downRightAttack]: "m",
+      }));
+    }
+
+    let downLeftAttack = leftMove.toString() + downMove;
+    if (
+      !ownPieces.includes(boardState[downLeftAttack]) &&
+      !attackPiece(downLeftAttack, isWhite)
+    ) {
+      setPossibleMoves((currentPossibleMoves) => [
+        ...currentPossibleMoves,
+        [downLeftAttack],
+      ]);
+      setBoardState((boardState) => ({
+        ...boardState,
+        [downLeftAttack]: "m",
+      }));
     }
   }
 
